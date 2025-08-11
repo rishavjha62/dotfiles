cb1ed4101aa83100c7ead9987e95e59cbf0f965b
Rishav Jha <risjha@ASF-VZN-0046UP.>
2025-08-12 00:21:30 +0530

    support for movement b/w panes

--- .config/nvim/lua/plugins/tmux.lua
+++ .config/nvim/lua/plugins/tmux.lua
@@ -4,6 +4,14 @@
 --
 return {
   "christoomey/vim-tmux-navigator",
+  lazy = false,
+  keys = {
+    { "<C-h>", "<cmd>TmuxNavigateLeft<cr>", desc = "Go to left pane" },
+    { "<C-j>", "<cmd>TmuxNavigateDown<cr>", desc = "Go to lower pane" },
+    { "<C-k>", "<cmd>TmuxNavigateUp<cr>", desc = "Go to upper pane" },
+    { "<C-l>", "<cmd>TmuxNavigateRight<cr>", desc = "Go to right pane" },
+    { "<C-\\>", "<cmd>TmuxNavigatePrevious<cr>", desc = "Go to previous pane" },
+  },
   cond = function()
     return os.getenv("TMUX") ~= nil
   end,
